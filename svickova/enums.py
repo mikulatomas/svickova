8e98f9e904e3150feb332c42b78ab0516f6fb956
Tomáš Petržela <info@thomasparsley.cz>
2022-04-16 10:24:09 +0200

    formating, typing, json dump self

--- svickova/enums.py
+++ svickova/enums.py
@@ -1,27 +1,25 @@
 from enum import Enum
 
-class Category(Enum):
-    SOUP = "Polévky, saláty"
-    DAILY_OFFER = "Hotovky"
-    SPECIALS = "Minutky"
+class Category(str, Enum):
+    SOUP: str        = "Polévky, saláty"
+    DAILY_OFFER: str = "Hotovky"
+    SPECIALS: str    = "Minutky"
 
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
+        return json.dumps(self)
 
-class Special(Enum):
-    FRY = "Smažené jídlo"
-    VEG = "Vegetariánské jídlo"
-    GLUTEN = "Bezlepkové jídlo"
+class Special(str, Enum):
+    FRY: str    = "Smažené jídlo"
+    VEG: str    = "Vegetariánské jídlo"
+    GLUTEN: str = "Bezlepkové jídlo"
 
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
+        return json.dumps(self)
 
-class Canteen(Enum):
-    LISTOPAD_17 = 1
-    KRIZKOVSKEHO = 2
-    SMERALOVA = 3
-    LEKARSKA_FAKULTA = 4
-    NEREDIN = 6
-    HOLICE = 13
+class Canteen(int, Enum):
+    LISTOPAD_17: int      = 1
+    KRIZKOVSKEHO: int     = 2
+    SMERALOVA: int        = 3
+    LEKARSKA_FAKULTA: int = 4
+    NEREDIN: int          = 6
+    HOLICE: int           = 13
