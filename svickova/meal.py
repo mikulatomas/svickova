5174472dae51d57b493649cad02f5719ef7939e1
Tomáš Petržela <info@thomasparsley.cz>
2022-04-16 17:41:37 +0200

    Added missing category 'Pizza' and json serializable (#2)
    * Added missing category 'Pizza'
    
    * typing, added pizza category
    
    * dataclass meal `to_json` method

--- svickova/meal.py
+++ svickova/meal.py
@@ -1,4 +1,6 @@
 import typing
+import json
+
 from dataclasses import dataclass
 
 from .enums import Category, Special
@@ -10,3 +12,27 @@ class Meal:
     special: typing.List["Special"]
     count: typing.Union[float, int]
     price: float
+
+    def __iter__(self):
+        count = self.count
+        price = self.price
+        
+        if count == float("inf") or count == float("-inf"):
+            count = 0
+
+        if price == float("inf") or price == float("-inf"):
+            price = 0
+
+        yield from {
+            "name": self.name,
+            "category": self.category.value,
+            "special": [s.value for s in self.special],
+            "count": count,
+            "price": price,
+        }.items()
+
+    def __str__(self) -> str:
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def to_json(self):
+        return dict(self)
